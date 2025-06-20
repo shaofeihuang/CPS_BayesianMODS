5824dfc21ddd5ecb5b3154ec80bb8d2ece77bb69
Huang Shaofei <166372696+shaofeihuang@users.noreply.github.com>
2025-06-20 15:14:56 +0800

    Add files via upload

--- AML_BBN_RailwayCBTC.py
+++ AML_BBN_RailwayCBTC.py
@@ -1,8 +1,8 @@
 '''
 #######################################################################################################
-Stuxnet Risk Assessment using Bayesian Belief Networks (BBN) based on AutomationML Models
+Railway CBTC Risk Assessment using Bayesian Belief Networks (BBN) based on AutomationML Models
 Author: Huang Shaofei
-Last update: 2025-06-06
+Last update: 2025-06-14
 #######################################################################################################
 '''
 
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     env = Environment(*setup_environment(interactive=True))
     aml_data = AMLData(*process_AML_file(env.element_tree_root, env.t))
 
+    #check_probability_data(aml_data)
+
     bbn_exposure = create_bbn_exposure(aml_data, env.sap)
     bbn_impact = create_bbn_impact(bbn_exposure, aml_data)
     check_bbn_models(bbn_exposure, bbn_impact)
@@ -19,8 +21,8 @@ if __name__ == "__main__":
     inference_exposure = VariableElimination(bbn_exposure)
     inference_impact = VariableElimination(bbn_impact)
 
-    #plot_bbn(bbn_exposure)
+    plot_bbn(bbn_exposure)
 
-    valid_nodes = {"user", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"}
-    source_node, target_node = find_shortest_path(bbn_exposure, valid_nodes, "User", "Centrifuge_Failure")
+    valid_nodes = {"attacker", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"}
+    source_node, target_node = find_shortest_path(bbn_exposure, valid_nodes, "Attacker", "H3_Disrupt_Train_Operation")
     compute_risk_scores(inference_exposure, inference_impact, aml_data.total_elements, source_node, target_node)
